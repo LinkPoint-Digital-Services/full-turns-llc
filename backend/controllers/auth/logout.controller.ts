8f389686cb997496920fa00e933fa0f76be77006
Joenel <joenelsevellejo831@gmail.com>
2026-01-29 10:01:00 +0800

    sameSite none

--- backend/controllers/auth/logout.controller.ts
+++ backend/controllers/auth/logout.controller.ts
@@ -5,7 +5,7 @@ export const logout = asynchandler(async (req: Request, res: Response) => {
   res.clearCookie("accessToken", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
   });
 
   res.status(200).json({ message: "Logged out successfully" });
