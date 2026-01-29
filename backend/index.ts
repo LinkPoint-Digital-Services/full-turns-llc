46fdec9e5cef6ca9a3069d1ba4b1d0ef9b910592
Joenel <joenelsevellejo831@gmail.com>
2026-01-29 09:52:31 +0800

    production err: cors

--- backend/index.ts
+++ backend/index.ts
@@ -5,22 +5,19 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectToMongoDB from './config/db';
 import router from './routes';
-import {NextFunction} from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 
-import express, {Request, Response} from 'express';
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://full-turns-llc.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://full-turns-llc.vercel.app'], // no trailing slash
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
-
-app.get('/', (req: Request, res: Response) => res.send('Express on Vercelss'));
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
@@ -28,13 +25,13 @@ app.options('*', cors(corsOptions));
 app.use(cookieParser());
 connectToMongoDB();
 
-app.use('/api', router); // Centralized route handling
+app.get('/', (req: Request, res: Response) => res.send('Express on Vercelss'));
+app.use('/api', router);
 
-//Centralized error handling middleware (for unexpected errors in routes)
+// Error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Something broke!';
-
   res.status(statusCode).json({message});
 });
 
