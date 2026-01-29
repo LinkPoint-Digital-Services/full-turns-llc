cea926257967e4d48d135351fed21eb466246018
Joenel <joenelsevellejo831@gmail.com>
2026-01-29 09:35:52 +0800

    remove hmt

--- backend/index.ts
+++ backend/index.ts
@@ -2,47 +2,42 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import cors from 'cors';
-import cookieParser from 'cookie-parser'; 
+import cookieParser from 'cookie-parser';
 import connectToMongoDB from './config/db';
 import router from './routes';
-import { NextFunction } from 'express';
-import helmet from "helmet";
+import {NextFunction} from 'express';
 
-import express, { Request, Response } from 'express'
-const app = express()
-const port = 5000
+import express, {Request, Response} from 'express';
+const app = express();
+const port = 5000;
 
 app.use(express.json());
-app.use(helmet());
 
 const corsOptions = {
-    origin: [
-      'http://localhost:3000',
-      'https://frontend-drab.vercel.app'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true 
+  origin: ['http://localhost:3000', 'https://full-turns-llc.vercel.app/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
+
+app.get('/', (req: Request, res: Response) => res.send('Express on Vercelss'));
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
-app.use(cookieParser())
+app.use(cookieParser());
 connectToMongoDB();
 
 app.use('/api', router); // Centralized route handling
 
 //Centralized error handling middleware (for unexpected errors in routes)
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Something broke!";
+  const message = err.message || 'Something broke!';
 
-  res.status(statusCode).json({ message });
+  res.status(statusCode).json({message});
 });
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
