c4c6b0fa77e2f3228288504c93fd9f64267f5b58
Joenel <joenelsevellejo831@gmail.com>
2026-06-21 20:22:13 +0800

    footer contacts modified

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -85,7 +85,7 @@ export default function Footer() {
               <div className="flex items-center gap-3 bg-primary/20 backdrop-blur-md rounded-full w-fit">
                 <Mail className="w-5 h-5 text-black" />
                 <span className="font-medium text-black text-[18px]">
-                  david@fullturns.com
+                  orders@fullturns.com
                 </span>
               </div>
 
